e7ec36a962e3df9e1efc5f03ae7968c847ec5bfc
sravan261 <77236850+sravan261@users.noreply.github.com>
2023-04-04 12:16:13 +0530

    working around basics

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
 
 const App = () => {
-  
+  const title = 'Blog Post';
+  const body = 'This is my blog post!';
+  const comments = [
+    {id:1, text:'Comment one'},
+    {id:2, text:'Comment two'},
+    {id:3, text:'Comment three'},
+    {id:4, text:'Comment four'}
+  ]
   return (
-    <h1>Hello from the App Componet!</h1>
+    <div className='container'>
+    <h1>{title.toUpperCase()}</h1>
+    <p>{body}</p>
+    {Math.random() * 5 * 5}
+    </div>
   )
 }
 
-export default App
+export default App 
